b74cbd441b753d8edb4dd40d30c0d308f1cb5450
roylic <roylic@wokoworks.com>
2023-03-06 13:31:39 +0800

    figure out how to use SolanaJ to find out associated account-token-address for related account

--- src/test/java/org/p2p/solanaj/own/OwnTryUsingTest.java
+++ src/test/java/org/p2p/solanaj/own/OwnTryUsingTest.java
@@ -6,6 +6,8 @@ import org.p2p.solanaj.core.PublicKey;
 import org.p2p.solanaj.rpc.RpcClient;
 import org.p2p.solanaj.rpc.RpcException;
 
+import java.util.Arrays;
+
 public class OwnTryUsingTest {
 
     private static RpcClient rpcClient = new RpcClient("https://api.devnet.solana.com");
@@ -20,6 +22,17 @@ public class OwnTryUsingTest {
     @Test
     public void decodeCheck() {
         System.out.println(Base58.decodeChecked("AnayTW335MabjhtXTJeBit5jdLhNeUVBVPXeRKCid79D"));
+    }
+
+    @Test
+    public void findTokenRelatedAddress() throws Exception {
+        PublicKey.ProgramDerivedAddress derivedAddress = PublicKey.findProgramAddress(
+                Arrays.asList(
+                        new PublicKey("AnayTW335MabjhtXTJeBit5jdLhNeUVBVPXeRKCid79D").toByteArray(),
+                        new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA").toByteArray(),
+                        new PublicKey("Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr").toByteArray()),
+                new PublicKey("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"));
+        System.out.println(derivedAddress.getAddress());
     }
 
 }
